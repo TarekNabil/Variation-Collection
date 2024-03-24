7915d3a056f371506e24457dbdf4944007e32e7c
TarekNabil <tarekthedeveloper@gmail.com>
2024-03-24 03:22:57 +0200

    using Class selection instead of ID

--- assets/js/variation_collection.js
+++ assets/js/variation_collection.js
@@ -3,7 +3,7 @@ jQuery( document ).ready( function( $ ) {
     $( ".single_variation_wrap" ).on( "show_variation", function ( event, variation ) {
     	var variation_id = variation.variation_id;
       	$('.custom_variations').hide();
-        $('#custom-variation-for-'+ variation_id).show();
-        $('#custom-variation-for-'+ variation_id+' ul li').css("visibility", "inherit").css("opacity", "inherit");
+        $('.custom-variation-for-'+ variation_id).show();
+        $('.custom-variation-for-'+ variation_id+' ul li').css("visibility", "inherit").css("opacity", "inherit");
     });
 });
